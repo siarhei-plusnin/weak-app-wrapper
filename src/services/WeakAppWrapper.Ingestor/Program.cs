62f17f05ad731254e7b1b25c581ef262864e1a7d
Siarhei Plusnin <siarhei.plusnin@gmail.com>
2026-06-13 13:54:20 +0300

    init services

---
+++ src/services/WeakAppWrapper.Ingestor/Program.cs
@@ -0,0 +1,5 @@
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+IHost host = builder.Build();
+
+host.Run();
---
+++ src/services/WeakAppWrapper.WebApi/Program.cs
@@ -0,0 +1,13 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.Run();
